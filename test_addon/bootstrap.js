b58e6160b7619ed8cf48c56dce4432c2a6e0c44c
avik <avikpal.me@gmail.com>
2013-06-17 09:56:59 +0530

    added startup() and shutdown()

--- test_addon/bootstrap.js
+++ test_addon/bootstrap.js
@@ -1,4 +1,6 @@
 //to check the current state of input buttons
+const Cc = Components.classes;
+const Ci = Components.interfaces;
 var menuID;
 function offerCake(window) {
   let buttons = [
@@ -33,4 +35,63 @@ function unloadFromWindow(window) {
     return;
   window.NativeWindow.menu.remove(menuID);  
 }
+
+var windowListener = {
+  onOpenWindow: function(aWindow) {
+    // Wait for the window to finish loading
+    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+    domWindow.addEventListener("load", function() {
+      domWindow.removeEventListener("load", arguments.callee, false);
+      loadIntoWindow(domWindow);
+    }, false);
+  },
+onCloseWindow: function(aWindow) {},
+onWindowTitleChange: function(aWindow, aTitle) {}
+};
+
+function startup(aData, aReason) {
+  //let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+  // Load into any existing windows
+  let windows = Services.wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    loadIntoWindow(domWindow);
+  }
+  // Load into any new windows
+  Services.wm.addListener(windowListener);
+}
+
+function shutdown(aData, aReason) {
+  // When the application is shutting down we normally don't have to clean
+  // up any UI changes made
+  if (aReason == APP_SHUTDOWN)
+    return;
+  // Stop listening for new windows
+  Services.wm.removeListener(windowListener);
+  // Unload from any existing windows
+  let windows = Services.wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    unloadFromWindow(domWindow);
+  }
+}
+
+function install(aData, aReason) {
+}
+
+function uninstall(aData, aReason) {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
