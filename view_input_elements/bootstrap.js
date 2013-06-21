340d34edc75ca227859a6a78e3b7ca132c4894d2
avik <avikpal.me@gmail.com>
2013-06-21 22:55:56 +0530

    :

---
+++ view_input_elements/bootstrap.js
@@ -0,0 +1,126 @@
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+function isNativeUI() {
+  return (Services.appinfo.ID == "{aa3c5121-dab2-40e2-81ca-7ea25febc110}");
+}
+
+function showToast(aWindow) {
+  aWindow.NativeWindow.toast.show("Showing you a toast", "short");
+}
+
+function showDoorhanger(aWindow) {
+  buttons = [
+    {
+      label: "Button 1",
+      callback: function() {
+        aWindow.NativeWindow.toast.show("Button 1 was tapped", "short");
+      }
+    } , {
+      label: "Button 2",
+      callback: function() {
+        aWindow.NativeWindow.toast.show("Button 2 was tapped", "short");
+      }
+    }];
+ //aWindow.NativeWindow.doorhanger.show("Showing a doorhanger with two button choices.", "doorhanger-test", buttons);
+aWindowNativeWindow.doorhanger.show("Hi", "hello", [ label: "ok" ], window.BrowserApp.selectedTab.id, {
+  inputs [
+    { type: "checkbox", id: "check", label: "Label" value: true },
+    { type: "input", id: "input", hint: "Enter something", value: "" },
+    { type: "password", id: "pass", hint: "Password!", value: "" },
+// HTML should work here...
+    { type: "label", id: "link: press me", value: "<b>Bold</b><a href='http://www.mozilla.org/en-US/'>Moz</a>" },
+    { type: "menulist", id: "menu", values: ["One", "Two"], label: "My values" },
+  ],
+});
+}
+
+function copyLink(aWindow, aTarget) {
+  let url = aWindow.NativeWindow.contextmenus._getLinkURL(aTarget);
+  aWindow.NativeWindow.toast.show("Todo: copy > " + url, "short");
+}
+
+var gToastMenuId = null;
+var gDoorhangerMenuId = null;
+var gContextMenuId = null;
+
+function loadIntoWindow(window) {
+  if (!window)
+    return;
+
+  if (isNativeUI()) {
+    //gToastMenuId = window.NativeWindow.menu.add("Show Toast", null, function() { showToast(window); });
+    gDoorhangerMenuId = window.NativeWindow.menu.add("Show Doorhanger", null, function() { showDoorhanger(window); });
+    //gContextMenuId = window.NativeWindow.contextmenus.add("Copy Link", window.NativeWindow.contextmenus.linkOpenableContext, function(aTarget) { copyLink(window, aTarget); });
+  }
+}
+
+function unloadFromWindow(window) {
+  if (!window)
+    return;
+
+  if (isNativeUI()) {
+    window.NativeWindow.menu.remove(gToastMenuId);
+    window.NativeWindow.menu.remove(gDoorhangerMenuId);
+    window.NativeWindow.contextmenus.remove(gContextMenuId);
+  }
+}
+
+
+/**
+ * bootstrap.js API
+ */
+var windowListener = {
+  onOpenWindow: function(aWindow) {
+    // Wait for the window to finish loading
+    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+    domWindow.addEventListener("load", function() {
+      domWindow.removeEventListener("load", arguments.callee, false);
+      loadIntoWindow(domWindow);
+    }, false);
+  },
+  
+  onCloseWindow: function(aWindow) {
+  },
+  
+  onWindowTitleChange: function(aWindow, aTitle) {
+  }
+};
+
+function startup(aData, aReason) {
+  // Load into any existing windows
+  let windows = Services.wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    loadIntoWindow(domWindow);
+  }
+
+  // Load into any new windows
+  Services.wm.addListener(windowListener);
+}
+
+function shutdown(aData, aReason) {
+  // When the application is shutting down we normally don't have to clean
+  // up any UI changes made
+  if (aReason == APP_SHUTDOWN)
+    return;
+
+  // Stop listening for new windows
+  Services.wm.removeListener(windowListener);
+
+  // Unload from any existing windows
+  let windows = Services.wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    unloadFromWindow(domWindow);
+  }
+}
+
+function install(aData, aReason) {
+}
+
+function uninstall(aData, aReason) {
+}
